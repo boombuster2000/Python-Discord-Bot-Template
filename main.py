7360fd39c4d26a41a3e503175c00598ccb1c717c
boombuster2000 <chanuka.williams@outlook.com>
2024-02-11 19:03:23 +0000

    Setup basic events and defined client.

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ if not config: print("Fill in the detials in {./config.json}.")
 
 intents = discord.Intents.default()
 intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(config["bot-token"])
