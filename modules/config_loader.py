8c5f55ae68a15e5dcf7c60f517cbaac266f299c0
boombuster2000 <chanuka.williams@outlook.com>
2024-02-11 17:42:55 +0000

    defined class for config_loader

--- modules/config_loader.py
+++ modules/config_loader.py
@@ -1,1 +1,13 @@
 import json
+
+class ConfigLoader():
+    def __init__(self, config_fp = "./config.json"):
+        self.CONFIG_TEMPLATE = {
+            "bot-token":"BOT-TOKEN"
+        }
+        self.CONFIG_FP = config_fp
+
+    
+
+        
+    
