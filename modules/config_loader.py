f1644c83ab136b81c000e638374acc96f2df25ca
boombuster2000 <chanuka.williams@outlook.com>
2024-02-11 20:17:11 +0000

    Corrected return values

--- modules/config_loader.py
+++ modules/config_loader.py
@@ -4,7 +4,8 @@ class ConfigLoader():
     def __init__(self, config_fp = "./config.json") -> None:
         self.CONFIG_TEMPLATE = {
             "bot-token":"BOT-TOKEN",
-            "dev-guild-id":0
+            "dev-guild-id":0,
+            "prop12":"test"
         }
         self.CONFIG_FP = config_fp
 
@@ -24,18 +25,19 @@ class ConfigLoader():
 
     def update_config_file(self, new_config):
         with open(self.CONFIG_FP, "w") as config_file:
-            json.dump(new_config, config_file)
+            json.dump(new_config, config_file, indent=4)
 
     def is_config_file_filled_in(self) -> bool: 
         config = self.load_config()
         for key in self.CONFIG_TEMPLATE.keys():
             try:
                 if config[key] == self.CONFIG_TEMPLATE[key]: 
-                    return True
+                    return False
             except KeyError:
                 config[key] = self.CONFIG_TEMPLATE[key]
                 self.update_config_file(config)
-                print(f"{key} not filled in.")
+                print(f"[{key}] not filled in.")
+                return False
             
-        return False
+        return True
     
