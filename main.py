5d8c9786b3c5c397d9c09e0432deb547b0d1a58e
boombuster2000 <chanuka.williams@outlook.com>
2024-02-11 18:37:18 +0000

    put config loading into seperate function that returns config file as dict or None if invalid.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from modules.config_loader import ConfigLoader
 
+def load_config():
+    try:
+        config = config_loader.load_config()
+        if not config_loader.is_config_file_filled_in(): return config
+
+        return None
+
+    except (FileNotFoundError):
+        config_loader.create_config_file()
+        return None
+
+
 config_loader = ConfigLoader()
+config = load_config()
+if not config: print("Fill in the detials in {./config.json}.")
 
-try:
-    config = config_loader.load_config()
-    if config_loader.is_config_file_filled_in(): print("Fill in details in config file.")
 
-except (FileNotFoundError):
-    config_loader.create_config_file()
-    print("Fill in details in config file.")
+
