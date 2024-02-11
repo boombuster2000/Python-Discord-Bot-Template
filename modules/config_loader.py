1e79135c613a9785eb9fc47d19836781df05f9ca
boombuster2000 <chanuka.williams@outlook.com>
2024-02-11 17:53:18 +0000

    copied functions to class

--- modules/config_loader.py
+++ modules/config_loader.py
@@ -7,7 +7,24 @@ class ConfigLoader():
         }
         self.CONFIG_FP = config_fp
 
-    
+    def create_config_file(self):
+        with open(self.CONFIG_FP, "w") as config_file:
+            json.dump(self.CONFIG_FILE_TEMPLATE, config_file, indent=4)
+            print("Fill in details in config file.")
 
-        
+    def load_config(self):
+        try:
+            with open(self.CONFIG_FP, "r") as config_file:
+                config = json.load(config_file)
+                return config
+        except (json.decoder.JSONDecodeError):
+            print("Invalid JSON in config file, resetting config file.")
+            self.create_config_file()
+
+    def is_config_file_filled_in(self, config):
+        for key in self.CONFIG_FILE_TEMPLATE.keys():
+            if config[key] == self.CONFIG_FILE_TEMPLATE[key]: 
+                return True
+            
+        return False
     
