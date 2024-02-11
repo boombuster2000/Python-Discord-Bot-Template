26ef69ebfd22191eba69c9dce5f1805e819e8a5f
boombuster2000 <chanuka.williams@outlook.com>
2024-02-11 18:38:26 +0000

    defined return types

--- modules/config_loader.py
+++ modules/config_loader.py
@@ -1,26 +1,27 @@
 import json
 
 class ConfigLoader():
-    def __init__(self, config_fp = "./config.json"):
+    def __init__(self, config_fp = "./config.json") -> None:
         self.CONFIG_TEMPLATE = {
             "bot-token":"BOT-TOKEN"
         }
         self.CONFIG_FP = config_fp
 
-    def create_config_file(self):
+    def create_config_file(self) -> None:
         with open(self.CONFIG_FP, "w") as config_file:
             json.dump(self.CONFIG_TEMPLATE, config_file, indent=4)
             
-    def load_config(self):
+    def load_config(self) -> dict | None:
         try:
             with open(self.CONFIG_FP, "r") as config_file:
                 config = json.load(config_file)
                 return config
+            
         except (json.decoder.JSONDecodeError):
             print("Invalid JSON in config file, resetting config file.")
             self.create_config_file()
 
-    def is_config_file_filled_in(self):
+    def is_config_file_filled_in(self) -> bool: 
         config = self.load_config()
         for key in self.CONFIG_TEMPLATE.keys():
             if config[key] == self.CONFIG_TEMPLATE[key]: 
