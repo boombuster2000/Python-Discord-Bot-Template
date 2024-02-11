49d661d7fe0b31d83b980e523bd2719ef034caa5
boombuster2000 <chanuka.williams@outlook.com>
2024-02-11 17:04:24 +0000

    created function to create config file

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import json
+
+
 def create_config_file(config_fp="./config.json"):
-    pass
+    CONFIG_FILE_TEMPLATE = {
+        "bot-token":"BOT-TOKEN"
+    }
+
+    with open(config_fp, "w") as config_file:
+        json.dump(CONFIG_FILE_TEMPLATE, config_file, indent=4)
+        print("Fill in details in config file.")
+
+create_config_file()
